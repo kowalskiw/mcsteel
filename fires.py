8d443adcd632bb8b30b944d9323651d7648731fb
kowalskiw_via_wsl <kowalskiwojciech986@gmail.com>
2021-01-28 17:54:22 +0100

    fires modified to SAFIR
    basic functions of mc.py implemented - not tested

---
+++ fires.py
@@ -0,0 +1,231 @@
+from numpy import random, pi
+from pandas import read_csv as rcsv
+from math import exp
+
+
+# triangular distribution sampler
+def triangular(left, right, mode=False):
+    if not mode:
+        mode = (right - left) / 3 + left
+    return random.triangular(left, mode, right)
+
+
+# drawn fire site among fuel area
+def mc_rand(csv):
+    ases = []   # list with partial factors A of each fuel area
+    probs = []  # list with probabilities of ignition in each fuel area
+
+    # calculate partial factor A (volume * probability) of each fuel area
+    for i, r in csv.iterrows():
+        a = (r['XB'] - r['XA']) * (r['YB'] - r['YA']) * (r['ZB'] - r['ZA']) * r['MC']
+        ases.append(a)
+
+    # calculate probability of ignition in each fuel area
+    for a in ases:
+        probs.append(a/sum(ases))
+
+    # return drawn fuel area
+    return random.choice(len(probs), p=probs), ases
+
+
+# drawn fire localization
+def f_localization(name):
+
+    ffile = rcsv('{}.ful'.format(name), sep=',')  # read FUL file
+    fire_site = mc_rand(ffile)[0]  # generate fire coordinates from MC function
+    config = ffile.iloc[fire_site]  # information about chosen fuel site
+
+    def random_position(xes, yes, zes):
+        coordinates = []
+        [coordinates.append(random.randint(int(10 * i[0]), int(10 * i[1])) / 10) for i in [xes, yes, zes]]
+        return coordinates
+
+    return config, random_position((config.XA, config.XB), (config.YA, config.YB), zes=(config.ZA, config.ZB))
+
+
+'''fire randomization class, which is recalled in CreateOZN.fire()'''
+
+
+class Properties:
+    def __init__(self, t_end):
+        self.t_end = t_end  # duration of simulation
+        self.hrr_max = 3e8  # W model limitation of HRR
+
+    # NOT WORKING - prepare to use
+
+    # def newzealand(self, name):
+    #     fuel_height = (0.32, 34.1)
+    #     fuel_xes = (0.3, 23.1)
+    #     fuel_yes = (10.3, 101.7)
+    #
+    #     H = fuel_height[1] - fuel_height[0]
+    #     A_max = (fuel_xes[1] - fuel_xes[0]) ** 2 * 3.1415 / 4
+    #
+    #     config = rcsv('{}.ful'.format(name), sep=',')
+    #     alpha = triangular(*config.alpha_min, *config.alpha_max, mode=float(config.alpha_mode))
+    #     area = triangular(0, A_max)
+    #
+    #     print('alpha:{}, radius: {}'.format(alpha, (area / 3.1415) ** 0.5))
+    #     hrr = []
+    #     for i in range(0, int(self.t_end/99)):
+    #         hrr.extend([i, round(H * alpha * (i ** 3) * 1000, 4)])  # [time /s/, HRR /W/]
+    #         if hrr[-1] > self.hrr_max:
+    #             hrr[-1] = self.hrr_max
+    #
+    #     return hrr, area, fuel_height, fuel_xes, fuel_yes
+    #
+    # def pool_fire(self, title, only_mass=False):
+    #     with open('{}.ful'.format(title)) as file:
+    #         fuel_prop = file.readlines()[1].split(',')
+    #
+    #     # random mass of fuel
+    #     try:
+    #         mass = triangular(int(fuel_prop[5]), int(fuel_prop[6]))
+    #     except ValueError:
+    #         mass = int(fuel_prop[5])
+    #
+    #     # random area of leakage
+    #     if only_mass:
+    #         area_ = mass * 0.03  # 0.019 # glycerol # 0.03 methanol leakage
+    #         area = triangular(area_ * 0.9 * 100, area_ * 1.1 * 100) / 100
+    #     else:
+    #         try:
+    #             area = triangular(int(fuel_prop[3]), int(fuel_prop[4]))
+    #         except ValueError:
+    #             area = int(fuel_prop[3])
+    #
+    #     if area < 0.28:
+    #         ml_rate = triangular(0.015 * .9, 0.015 * 1.1)
+    #     elif area < 7.07:
+    #         ml_rate = triangular(0.022 * .9, 0.022 * 1.1)
+    #     else:
+    #         ml_rate = triangular(0.029 * .9, 0.029 * 1.1)
+    #
+    #     if self.a_max < area:
+    #         area = self.a_max
+    #
+    #     print('mass loss rate = {}'.format(ml_rate))
+    #     hrr_ = float(fuel_prop[1]) * ml_rate * area  # [MW] - heat release rate
+    #     hrr = triangular(hrr_ * .8, hrr_ * 1.2)
+    #
+    #     time_end = mass / ml_rate / area
+    #     if time_end > self.t_end:
+    #         time_end = self.t_end
+    #         hrr_list = [0, hrr, time_end / 60, hrr]
+    #     else:
+    #         if time_end < 60:
+    #             time_end = 60
+    #         hrr_list = [0, hrr, time_end / 60, hrr]
+    #         hrr_list.extend([hrr_list[-2] + 1 / 6, 0, self.t_end / 60, 0])
+    #
+    #     print('HRR = {}MW'.format(hrr))
+    #
+    #     fuel_h = round(1 / float(fuel_prop[2]) / float(fuel_prop[5]), 2)
+    #
+    #     return hrr_list, area, fuel_h
+
+    def test_fire(self):
+        hrr = [0, 0, 15, 40]
+        area = 10
+        height = 0
+
+        return hrr, area, height
+
+    # t-squared fire
+    def alfa_t2(self, name, property=None):
+        config, f_coordinates = f_localization(name)
+
+        # calculate HRRPUA according to triangular distribution specified by user
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000     # kW/m2
+
+        # calculate ALPHA according to the experimental log-norm or user's triangular distribution
+        if not property:
+            alpha = triangular(config.alpha_min, config.alpha_max, mode=config.alpha_mode)      # kW/s2
+        elif property == 'store':
+            alpha = hrrpua * random.lognormal(-9.72, 0.97)       # kW/s2
+
+        hrr_tab = []
+        diam_tab = []
+        for i in range(0, 100):
+            t = int(self.t_end * i/99)
+
+            # calculate HRR, append
+            hrr_tab.extend([t, round(alpha * (t ** 2) * 1000, 4)])  # [time /s/, HRR /W/]
+            if hrr_tab[-1] > self.hrr_max:    # check if hrr_tab does not exceed model limitation
+                hrr_tab[-1] = self.hrr_max
+
+            # calculate diameter, append
+            diam_tab.extend([t, 2 * (hrr_tab[-1] / hrrpua/pi)**0.5])  # [time /s/, diameter /m/]
+
+        return hrr_tab, diam_tab, f_coordinates, hrrpua, alpha
+
+    # curve taking non-effective sprinklers into account
+    def sprink_noeff(self, name, property=None):
+        config, f_coordinates = f_localization(name)
+
+        # calculate HRRPUA according to triangular distribution specified by user
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000  # [kW]
+
+        # calculate ALPHA according to the experimental log-norm or user's triangular distribution
+        if not property:
+            alpha = triangular(config.alpha_min, config.alpha_max, mode=config.alpha_mode)  # [kW/s2]
+        elif property == 'store':
+            alpha = hrrpua * random.lognormal(-9.72, 0.97)   # [kW/s2]
+
+        hrr_tab = []
+        diam_tab = []
+        for i in range(0, 100):
+            t = int(self.t_end * i/99)
+
+            # calculate HRR (steady-state after sprinklers activation), append
+            if t < config.t_sprink:
+                hrr_tab.extend([t, round(alpha * (t ** 2) * 1000, 4)])  # [time /s/, HRR /W/]
+            else:
+                hrr_tab.extend([t, round(alpha * (config.t_sprink ** 2) * 1000, 4)])  # [time /s/, HRR /W/]
+
+            if hrr_tab[-1] > self.hrr_max:  # check if hrr_tab does not exceed model limitation
+                hrr_tab[-1] = self.hrr_max
+
+            # calculate diameter, append
+            diam_tab.extend([t, 2 * (hrr_tab[-1] / hrrpua/pi)**0.5])  # [time /s/, diameter /m/]
+
+        return hrr_tab, diam_tab, f_coordinates, hrrpua, alpha
+
+    # curve taking effective sprinklers into account
+    def sprink_eff(self, name, property=None):
+        config, f_coordinates = f_localization(name)
+
+        # calculate HRRPUA according to triangular distribution specified by user
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000  # [kW]
+
+        # calculate ALPHA according to the experimental log-norm or user's triangular distribution
+        if not property:
+            alpha = triangular(config.alpha_min, config.alpha_max, mode=config.alpha_mode)  # [kW/s2]
+        elif property == 'store':
+            alpha = hrrpua * random.lognormal(-9.72, 0.97)   # [kW/s2]
+
+        hrr_tab = []
+        diam_tab = []
+        q_0 = alpha * config.t_sprink ** 2 * 1000  # [W]
+        q_limit = round(0.15 * q_0)
+        for i in range(0, 100):
+            t = int(self.t_end * i/99)
+
+            # calculate HRR (extinguishing phase after sprinklers activation), append
+            if t < config.t_sprink:
+                hrr_tab.extend([t, round(alpha * (t ** 2) * 1000, 4)])  # [time /s/, HRR /W/]
+            else:
+                q = round(q_0 * exp(-0.0024339414 * (t - config.t_sprink)), 4)  # W
+                if q >= q_limit:
+                    hrr_tab.extend([t, q])  # [time /s/, HRR /W/]
+                    hrr_tab.extend([round(i, 4) for i in [t / 60, q]])
+                else:
+                    hrr_tab.extend([round(i, 4) for i in [t / 60, q_limit]])
+
+            if hrr_tab[-1] > self.hrr_max:  # check if hrr_tab does not exceed model limitation
+                hrr_tab[-1] = self.hrr_max
+
+            # calculate diameter, append
+            diam_tab.extend([t, 2 * (hrr_tab[-1] / hrrpua/pi)**0.5])  # [time /s/, diameter /m/]
+
+        return hrr_tab, diam_tab, f_coordinates, hrrpua, alpha
--- mc.py
+++ mc.py
@@ -1,43 +1,177 @@
-from os import listdir, getcwd, chdir, popen, mkdir, path, replace
-import json as js
-from pynput.keyboard import Key, Controller
-import time
-from sys import argv
+from time import time as current_seconds
 import numpy as np
-from numpy import sqrt, log, random, pi
-from datetime import datetime as dt
-from export import Export
-from fires import Fires
+from fires import Properties
 import ezdxf
 import shapely.geometry as sh
+from pandas import DataFrame as df
 
 
 '''Read geometry and map it to the fire (choose the most exposed sections)'''
 
 
 class Single:
-    def __init__(self):
-        pass
+    def __init__(self, title, element_type='b'):
+        self.title = title     # simulation title
+        self.f_type = ''    # type of fire
+        self.t_end = 0      # simulation duration time
+        self.element_type = element_type  # 'b' - beam, 'c' - column
 
-    # import geometry data
+    # read dxf geometry
     def read_dxf(self):
-        pass
+        def dxf_to_shapely(dxfshells):    # convert shells to shapely objects
+            shshells = {}
 
-    # map fire to geometry - choose fire scenario
-    def map(self):
-        pass
+            for s in dxfshells:
+                shshell = sh.Polygon([s[0], s[1], s[2], s[3]])
+                shshells[str(s[0][-1])] = shshell
 
+            return shshells
 
+        dxffile = ezdxf.readfile('{}.dxf'.format(self.title))
+        msp = dxffile.modelspace()
+        columns = []
+        beams = []
+        for l in msp.query('LINE'):     # assign LINES elements to columns or beams tables
+            if l.dxf.start[:-1] == l.dxf.end[:-1]:
+                columns.append(l)
+            else:
+                beams.append(l)
 
-# import fire coordinates, HRR-time and Diameter-time tables
+        # assign 3DFACE elements to shells table
+        shells = []
+        [shells.append(s) for s in msp.query('3DFACE')]
+
+        return beams, columns, dxf_to_shapely(shells)
+
+    # import fire properties
+    def f_properties(self):
+
+        # fire area is limited only by model limitation implemented to the fires.Properties
+
+        f = Properties(self.t_end)
+        if self.f_type == 'alfat2':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.alfa_t2(self.title)
+        elif self.f_type == 'alfat2_store':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.alfa_t2(self.title, property='store')
+        elif self.f_type == 'sprink-eff':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.sprink_eff(self.title)
+        elif self.f_type == 'sprink-eff_store':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.sprink_eff(self.title, property='store')
+        elif self.f_type == 'sprink-noeff':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.sprink_noeff(self.title)
+        elif self.f_type == 'sprink-noeff_store':
+            f_hrr, f_diam, f_coordinates, hrrpua, alpha = f.sprink_noeff(self.title, property='store')
+        else:
+            raise(KeyError, '{} is not a proper fire type'.format(self.f_type))
+
+        return f_coordinates, f_hrr, f_diam
+
+    # map fire and geometry - choose fire scenario
+    def map(self, f_coords, elements, element):
+        # select the most exposed section among the lines and return its properties
+        def map_lines(lines):
+            d = 1e9  # infinitely large number
+            index = None
+
+            # return vectors for further calculations (line_start[0], line_end[1], fire[2], es[3], fs[4], fe[5], se[6])
+            # find references
+            def vectors(line):
+                l_start = np.array(line.dxf.start)
+                l_end = np.array(line.dxf.end)
+                fire = np.array(f_coords)
+
+                return l_start, l_end, fire, l_end - l_start, fire - l_start, fire - l_end, l_start - l_end
+
+            # iterate over lines to select the closest to the fire one
+            for l in lines:
+                v = vectors(l)
+
+                # orthogonal projection module
+                def cos_vec(v1, v2):
+                    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+                if cos_vec(v[3], v[4]) >= 0 and cos_vec(v[6], v[5]) >= 0:  # choose projected point
+                    d_iter = np.linalg.norm(np.cross(v[3], v[4])) / np.linalg.norm(v[3])
+                else:  # choose the nearest edge if not
+                    d_iter = min([np.linalg.norm(v[2] - v[0]), (np.linalg.norm(v[2] - v[0]))])
+
+                # overwrite with analysed line if it is closer to the fire then the already chosen
+                if d_iter < d:
+                    d = d_iter
+                    index = lines.index(l)
+
+            v = vectors(lines[index])  # generate vectors for selected line
+            section = v[0] + (np.dot(v[4], v[3]) / np.dot(v[3], v[3])) * v[3]  # find the most exposed section coords
+
+            # set column's section to the biggest heat flux height (1.2m from the fire base)
+            if element == 'c':
+                # check if addition 1.2 m to the section Z is possible
+                if section[-1] + 1.2 < max([v[1][-1], v[0][-1]]):
+                    section += [0, 0, 1.2]
+                else:
+                    section[-1] = max([v[1][-1], v[0][-1]])
+
+            fire_relative = section - fire  # calculate fire-section vector
+
+            # fire coords, section coords, length of the fire-section vector, level of shell above the fire, profile
+            return fire, section, d, shell_lvl, lines[index].dxf.layer
+
+        # remove elements beneath the fire base or above shell level from the lines
+        def cut_lines(lines):
+            for l in lines:
+                if l.dxf.start[2] > shell_lvl or l.dxf.end[2] < f_coords[2]:
+                    lines.remove(l)
+            return lines
+
+        # import fire and structure data
+        beams, columns, shells = elements  # shells -> dict{Z_level:Plygon} | lines
+        fire = sh.Point(list(f_coords))  # shapely does not support the 3D objects - z coordinate is not used
+        shell_lvl = 1e9     # infinitely large number
+
+        # check for shell (plate, ceiling) existing above the fire assign the level if true
+        for lvl, poly in shells.items():
+            lvl = float(lvl)
+            if float(f_coords[2]) <= lvl < shell_lvl and poly.contains(fire):
+                shell_lvl = lvl
+        if shell_lvl == 1e6:  # set shell_lvl as -1 when there is no plate above the fire (besides compartment ceiling)
+            shell_lvl = -1
+
+        # initialize mapping
+        if element == 'b':  # cut beams accordingly to Z in (fire_z - shell_lvl) range and map to relative
+            mapped = map_lines(cut_lines(beams))
+        elif element == 'c':  # cut columns accordingly to Z in (fire_z - shell_lvl) range and map to relative
+            mapped = map_lines(cut_lines(columns))
+        else:
+            raise ValueError('{} is not a proper element type'.format(element))
+
+        self.prof_type = mapped[3]
+
+        # # returns tuple (x_r, y_r, z_r, [distance3D, LOCAFI_h, 'h', profile, shell height])
+        # return (*mapped[0], [mapped[1], mapped[2] - fire_z, element, mapped[3], mapped[2]])
+
+        # (fire coords, section coords, length of the fire-section vector, level of shell above the fire, profile)
+        return mapped
+
+    def generate(self): return self.map(self.f_properties()[0], self.read_dxf(), element=self.element_type)
 
 
 # generates set of n scenarios
-def generate_set(n):
-    pass
+def generate_set(n, title):
+    csvset = df(columns=('ID', 'x_f', 'y_f', 'z_f', 'x_s', 'y_s', 'z_s', 'distance', 'ceiling_lvl', 'profile'))
+    simid_core = int(current_seconds())
 
-# generates initial files (elem.in, prof.in, locafi.txt)
+    for i in range(n*2, step=2):  # add MC-drawn records to the DataFrame
+        csvset.loc[i] = [simid_core+i] + list(Single(title).generate())
+        csvset.loc[i+1] = [simid_core+i] + list(Single(title, element_type='c').generate())
 
+    csvset.to_csv('{}_set.csv'.format(title))    # save to the csv file
+
+
+# generates initial files (elem.in, prof.in, locafi.txt) based on DataFrame row (title_set.csv)
 def generate_in(sim_data):
-   pass
+    # create simulation directory
+    # copy profile GiD directory
+    # create dummy.in (for one element S3D - just to map fire to element in SAFIR)
+    # generate locafi.txt file
+    pass
 
