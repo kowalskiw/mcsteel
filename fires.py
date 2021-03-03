341f16bb39912a8c0b3dae0f9dbf88bd3df1600f
kowalskiw_via_wsl <kowalskiwojciech986@gmail.com>
2021-03-03 13:03:31 +0100

    modified for new fuel config
    example updated

--- fires.py
+++ fires.py
@@ -16,9 +16,9 @@ def mc_rand(csv):
     ases = []   # list with partial factors A of each fuel area
     probs = []  # list with probabilities of ignition in each fuel area
 
-    # calculate partial factor A (volume * probability) of each fuel area
+    # calculate partial factor A (area * probability) of each fuel area
     for i, r in csv.iterrows():
-        a = (r['XB'] - r['XA']) * (r['YB'] - r['YA']) * (r['ZB'] - r['ZA']) * r['MC']
+        a = (r['XB'] - r['XA']) * (r['YB'] - r['YA']) * r['MC']
         ases.append(a)
 
     # calculate probability of ignition in each fuel area
@@ -26,7 +26,7 @@ def mc_rand(csv):
         probs.append(a/sum(ases))
 
     # return drawn fuel area
-    return random.choice(len(probs), p=probs), ases
+    return random.choice(len(probs), p=probs)
 
 
 # drawn fire localization
@@ -36,7 +36,7 @@ def f_localization(title, config='fuel&stp'):
         ffile = Fuel(title).read_fuel()  # read FUL file
     else:
         ffile = read_csv('{}.ful'.format(title))
-    fire_site = mc_rand(ffile)[0]  # generate fire coordinates from MC function
+    fire_site = mc_rand(ffile)  # generate fire coordinates from MC function
     config = ffile.iloc[fire_site]  # information about chosen fuel site
 
     def random_position(xes, yes, zes):
@@ -130,7 +130,6 @@ class Fuel:
                                     't_sprink'])
 
         fuel = read_csv('{}.fuel'.format(self.title))
-
         for v in vols:
             data = fuel[fuel.name == v[0]]
             # convert list to DF
@@ -146,7 +145,6 @@ class Fuel:
         for v in vols:
             pts = self.find_points(v)
             fuel.append([self.layer(v.ref, lays), self.pts2fds(pts)])
-
         # merge with .FUL config type
         return self.merge_data(fuel)
 
@@ -160,79 +158,6 @@ class Properties:
         self.t_end = t_end  # duration of simulation
         self.hrr_max = 3e8  # W model limitation of HRR
 
-    # NOT WORKING - prepare to use
-
-    # def newzealand(self, name):
-    #     fuel_height = (0.32, 34.1)
-    #     fuel_xes = (0.3, 23.1)
-    #     fuel_yes = (10.3, 101.7)
-    #
-    #     H = fuel_height[1] - fuel_height[0]
-    #     A_max = (fuel_xes[1] - fuel_xes[0]) ** 2 * 3.1415 / 4
-    #
-    #     config = rcsv('{}.ful'.format(name), sep=',')
-    #     alpha = triangular(*config.alpha_min, *config.alpha_max, mode=float(config.alpha_mode))
-    #     area = triangular(0, A_max)
-    #
-    #     print('alpha:{}, radius: {}'.format(alpha, (area / 3.1415) ** 0.5))
-    #     hrr = []
-    #     for i in range(0, int(self.t_end/99)):
-    #         hrr.extend([i, round(H * alpha * (i ** 3) * 1000, 4)])  # [time /s/, HRR /W/]
-    #         if hrr[-1] > self.hrr_max:
-    #             hrr[-1] = self.hrr_max
-    #
-    #     return hrr, area, fuel_height, fuel_xes, fuel_yes
-    #
-    # def pool_fire(self, title, only_mass=False):
-    #     with open('{}.ful'.format(title)) as file:
-    #         fuel_prop = file.readlines()[1].split(',')
-    #
-    #     # random mass of fuel
-    #     try:
-    #         mass = triangular(int(fuel_prop[5]), int(fuel_prop[6]))
-    #     except ValueError:
-    #         mass = int(fuel_prop[5])
-    #
-    #     # random area of leakage
-    #     if only_mass:
-    #         area_ = mass * 0.03  # 0.019 # glycerol # 0.03 methanol leakage
-    #         area = triangular(area_ * 0.9 * 100, area_ * 1.1 * 100) / 100
-    #     else:
-    #         try:
-    #             area = triangular(int(fuel_prop[3]), int(fuel_prop[4]))
-    #         except ValueError:
-    #             area = int(fuel_prop[3])
-    #
-    #     if area < 0.28:
-    #         ml_rate = triangular(0.015 * .9, 0.015 * 1.1)
-    #     elif area < 7.07:
-    #         ml_rate = triangular(0.022 * .9, 0.022 * 1.1)
-    #     else:
-    #         ml_rate = triangular(0.029 * .9, 0.029 * 1.1)
-    #
-    #     if self.a_max < area:
-    #         area = self.a_max
-    #
-    #     print('mass loss rate = {}'.format(ml_rate))
-    #     hrr_ = float(fuel_prop[1]) * ml_rate * area  # [MW] - heat release rate
-    #     hrr = triangular(hrr_ * .8, hrr_ * 1.2)
-    #
-    #     time_end = mass / ml_rate / area
-    #     if time_end > self.t_end:
-    #         time_end = self.t_end
-    #         hrr_list = [0, hrr, time_end / 60, hrr]
-    #     else:
-    #         if time_end < 60:
-    #             time_end = 60
-    #         hrr_list = [0, hrr, time_end / 60, hrr]
-    #         hrr_list.extend([hrr_list[-2] + 1 / 6, 0, self.t_end / 60, 0])
-    #
-    #     print('HRR = {}MW'.format(hrr))
-    #
-    #     fuel_h = round(1 / float(fuel_prop[2]) / float(fuel_prop[5]), 2)
-    #
-    #     return hrr_list, area, fuel_h
-
     def test_fire(self):
         hrr = [0, 0, 15, 40]
         area = 10
@@ -245,13 +170,13 @@ class Properties:
         config = f_localization(name)[0]
 
         # calculate HRRPUA according to triangular distribution specified by user
-        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000     # kW/m2
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode)     # [kW/m2]
 
         # calculate ALPHA according to the experimental log-norm or user's triangular distribution
         if not property:
-            alpha = triangular(config.alpha_min, config.alpha_max, mode=config.alpha_mode)      # kW/s2
+            alpha = triangular(config.alpha_min, config.alpha_max, mode=config.alpha_mode)      # [kW/s2]
         elif property == 'store':
-            alpha = hrrpua * random.lognormal(-9.72, 0.97)       # kW/s2
+            alpha = hrrpua * random.lognormal(-9.72, 0.97)       # [kW/s2]
 
         hrr_tab = []
         diam_tab = []
@@ -273,7 +198,7 @@ class Properties:
         config = f_localization(name)[0]
 
         # calculate HRRPUA according to triangular distribution specified by user
-        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000  # [kW]
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode)  # [kW/m2]
 
         # calculate ALPHA according to the experimental log-norm or user's triangular distribution
         if not property:
@@ -305,7 +230,7 @@ class Properties:
         config = f_localization(name)[0]
 
         # calculate HRRPUA according to triangular distribution specified by user
-        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode) * 1000  # [kW]
+        hrrpua = triangular(config.hrrpua_min, config.hrrpua_max, mode=config.hrrpua_mode)  # [kW]
 
         # calculate ALPHA according to the experimental log-norm or user's triangular distribution
         if not property:
